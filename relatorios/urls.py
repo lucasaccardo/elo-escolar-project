ceb9123986b2afa0c117323aca7dab7599a846b7
Lucas Mateus Sureira <lucasmateussureira1999@gmail.com>
2026-09-11 15:51:33 -0300

    Adiciona rota raiz para a listagem de relatorios

--- relatorios/urls.py
+++ relatorios/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.lista_relatorios, name='inicio'),
     path('relatorios/', views.lista_relatorios, name='lista_relatorios'),
     path('relatorios/novo/', views.cadastrar_relatorio, name='cadastrar_relatorio'),
 ]
