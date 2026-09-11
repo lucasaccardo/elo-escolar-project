149f1efba73d5163dbde6b839be7426cf1483051
Lucas Mateus Sureira <lucasmateussureira1999@gmail.com>
2026-09-11 08:47:33 -0300

    Cria models Turma e RelatorioDiario com registro no admin

--- relatorios/admin.py
+++ relatorios/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Turma, RelatorioDiario
 
-# Register your models here.
+@admin.register(Turma)
+class TurmaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'ano_letivo')
+
+@admin.register(RelatorioDiario)
+class RelatorioDiarioAdmin(admin.ModelAdmin):
+    list_display = ('turma', 'data_aula', 'autor', 'publicado_em')
+    list_filter = ('turma',)
+    search_fields = ('conteudo', 'tarefa_descricao')
--- relatorios/models.py
+++ relatorios/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+
+class Turma(models.Model): #Para representar uma turma de alunos
+    nome = models.CharField(max_length=100)
+    ano_letivo = models.IntegerField()
+
+    def __str__(self):
+        return self.nome
+
+
+class RelatorioDiario(models.Model): #Para representar o relatório diário de uma turma
+    turma = models.ForeignKey(Turma, on_delete=models.PROTECT)
+    data_aula = models.DateField()
+    conteudo = models.TextField()
+    tarefa_descricao = models.TextField(blank=True)
+    tarefa_data_entrega = models.DateField(null=True, blank=True)
+    observacoes = models.TextField(blank=True)
+    autor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    publicado_em = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-data_aula']
+
+    def __str__(self):
+        return f'{self.turma} - {self.data_aula}'
