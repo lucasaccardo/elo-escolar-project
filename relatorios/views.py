7e66a1f181dedf3b54a1434d953186f03f564c8e
Lucas Mateus Sureira <lucasmateussureira1999@gmail.com>
2026-09-14 01:05:06 -0300

    Adiciona filtro de relatorios por turma

--- relatorios/views.py
+++ relatorios/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render, redirect
-from .models import RelatorioDiario
-from.forms import RelatorioDiarioForm
-
+from .models import RelatorioDiario, Turma
+from .forms import RelatorioDiarioForm
 
 def lista_relatorios(request):
-    # Por enquanto, busca todos os relatórios do banco de dados. 
+    # A ordenação padrão (-data_aula) já é garantida pelo class Meta do model
     relatorios = RelatorioDiario.objects.all()
     
-    # Renderiza o template passando o dicionário de contexto
-    return render(request, 'relatorios/lista_relatorios.html', {'relatorios': relatorios})
+    # Busca todas as turmas e ordena em ordem alfabética para o seletor
+    turmas = Turma.objects.all().order_by('nome')
+    
+    # Lê o parâmetro 'turma' da URL (ex: ?turma=1)
+    turma_id = request.GET.get('turma')
+    
+    # Se vier uma turma selecionada, aplica o filtro na consulta
+    if turma_id:
+        relatorios = relatorios.filter(turma_id=turma_id)
+    
+    # Renderiza o template passando o contexto completo
+    return render(request, 'relatorios/lista_relatorios.html', {
+        'relatorios': relatorios,
+        'turmas': turmas,
+        'turma_selecionada': turma_id,
+    })
 
 def cadastrar_relatorio(request):
     if request.method == 'POST':
