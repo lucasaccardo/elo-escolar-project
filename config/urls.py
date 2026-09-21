e3051d59da448009cfcbff4dc5ac98f2bf1bb290
Lucas Mateus Sureira <lucasmateussureira1999@gmail.com>
2026-09-21 16:20:14 -0300

    Redireciona usuario logado que abre a tela de login

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(
+        'contas/login/',
+        auth_views.LoginView.as_view(redirect_authenticated_user=True),
+        name='login',
+    ),
     path('contas/', include('django.contrib.auth.urls')),
     path('acessos/', include('acessos.urls')),
     path('', include('relatorios.urls')),
